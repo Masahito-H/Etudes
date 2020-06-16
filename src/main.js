f1c14f292eccb22a84e618d2dcf9f1ad0589a2eb
Masahito-H <masahito.hayashi.8p@gmail.com>
2020-06-16 19:19:43 +0900

    via atom

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import store from "./store.js";
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+const app = new Vue({
   el: '#app',
   router,
   store,
   render: h => h(App)
 })
+
+app.$mount("app");
